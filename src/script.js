1873f4a7aa6201cdebdc2597d601c6bddd3be5b9
Ezuri6 <carlotacharlot@hotmail.com>
2024-11-15 14:29:38 +0000

    more js to temperature
    needs organization and fixing yet

--- src/script.js
+++ src/script.js
@@ -1,10 +1,22 @@
-function findRealInf(){
-  let citySearch = document.querySelector("#search");
-  let city = citySearch.value;
+
+
+function findTemp (response){
+    let temperature = document.querySelector("#temp-n");
+
+    temperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+
+
+
+
+function findRealInf(city){
+  
+  let city = response.data.
   let apiKey = "49590o9129b5cbb04f9d3323t6a164fe";
   let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
    
-  
+  axios.get(apiUrl).then(findTemp);
 
 }
 
@@ -12,10 +24,11 @@ function findRealInf(){
 
 function findCity(event) {
 event.preventDefault();
+let citySearch = document.querySelector("#search");
 let cityElement = document.querySelector("#city-name");
 cityElement.innerHTML = citySearch.value;
 
-axios.get(apiUrl).then(findRealInf);
+
 }
 
 
