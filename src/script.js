5357f803831e835c6e62cce3572ac9b3c1a0e040
Ezuri6 <carlotacharlot@hotmail.com>
2024-11-16 14:39:13 +0000

    real inf js, moving things around

--- src/script.js
+++ src/script.js
@@ -2,17 +2,15 @@
 
 function findTemp (response){
     let temperature = document.querySelector("#temp-n");
-
+    let cityElement = document.querySelector("#city-name");
+    cityElement.innerHTML = response.data.city;
     temperature.innerHTML = Math.round(response.data.temperature.current);
 }
 
 
-
-
-
 function findRealInf(city){
   
-  let city = response.data.
+  
   let apiKey = "49590o9129b5cbb04f9d3323t6a164fe";
   let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
    
@@ -25,8 +23,8 @@ function findRealInf(city){
 function findCity(event) {
 event.preventDefault();
 let citySearch = document.querySelector("#search");
-let cityElement = document.querySelector("#city-name");
-cityElement.innerHTML = citySearch.value;
+
+
 
 
 }
@@ -34,3 +32,4 @@ cityElement.innerHTML = citySearch.value;
 
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", findCity);
+findRealInf("Cadiz");
