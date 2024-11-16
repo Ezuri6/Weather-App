ed8b2fc21ac3a51312f24ffbd9edd70209ead8a4
Ezuri6 <carlotacharlot@hotmail.com>
2024-11-16 18:47:57 +0000

    first&second function organised js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
 
-function findTemp (response){
+function findAllDetails (response){
     let temperature = document.querySelector("#temp-n");
     let cityElement = document.querySelector("#city-name");
     cityElement.innerHTML = response.data.city;
@@ -9,26 +9,18 @@ function findTemp (response){
 
 
 function findRealInf(city){
-  
-  
   let apiKey = "49590o9129b5cbb04f9d3323t6a164fe";
   let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
    
-  axios.get(apiUrl).then(findTemp);
-
+  axios.get(apiUrl).then(findAllDetails);
 }
-
-
 
 function findCity(event) {
 event.preventDefault();
 let citySearch = document.querySelector("#search");
 
-
-
-
+findRealInf(citySearch.value);
 }
-
 
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", findCity);
